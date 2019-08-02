b3bc286abf040810d08c6bdcc89c255c189bec89
Michael Kim <m.kim4247@gmail.com>
2019-08-02 14:38:43 -0400

    adding landing nav

--- src/components/LandingPage/Landing.js
+++ src/components/LandingPage/Landing.js
@@ -4,10 +4,12 @@ import Info from './Info'
 import Extra from './Extra'
 import Album from './Album'
 import Footer from './Footer'
+import LandingNav from './LandingNav'
 
 const Landing = () => {
   return(
     <div>
+      <LandingNav />
       <Hero />
       <Info />
       <Extra />
---
+++ src/components/LandingPage/LandingNav.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { NavLink } from 'react-router-dom'
+import { Menu } from 'semantic-ui-react'
+
+class LandingNav extends React.Component {
+
+  render(){
+    return(
+      <div>
+        <Menu inverted color={'teal'}>
+          {this.props.user ?
+            <Menu.Item
+              as={NavLink}
+              to={`/user/${this.props.user.username}`}>
+                {this.props.user.first_name[0] + this.props.user.last_name[0]}
+            </Menu.Item>
+            : null
+          }
+          {this.props.user ?
+            <Menu.Item
+              as={NavLink}
+              to='/boards'
+              name="Boards" />
+            : null
+          }
+          <Menu.Item
+            as={NavLink}
+            to='/about'
+            name='About' />
+          <Menu.Menu position='right'>
+            {this.props.user ?
+              <Menu.Item
+                as={NavLink}
+                name="Logout"
+                to='/login'
+                onClick={this.logout} />
+              :
+              <Menu.Item
+                name="Login"
+                as={NavLink}
+                to='/login'/>
+            }
+          </Menu.Menu>
+        </Menu>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    user: state.user
+  }
+}
+
+export default connect(mapStateToProps)(LandingNav)
